b5e426ce74a928ae3b90588c65ef56cf54741e73
Srini Talasani <SriniTalasani@gmail.com>
2026-04-23 11:53:31 +0200

    new amazon test

--- src/test/features/amazonWebsite.feature
+++ src/test/features/amazonWebsite.feature
@@ -1,14 +1,20 @@
 @amazon
 Feature: Launch Amazon website
+Background: 
+Given I opened amazon website
 
 Scenario: Launch amazon and verify text
-Given I opened amazon website
 Then I verify update location text
+
+Scenario: Navigate to grocery and add product to cart
 When I click on Everyday essentials
-Then I should be able to see Grocery
-When I click on Grocery
-Then I should be able to see Grocery page
+Then I should be able to see Groceries
+When I click on Groceries
+Then I should be navigated to Grocery page
 When I select the product 
 Then I should be navigated to product details page
 When I click on add to cart button
 Then I should be able to see the product in cart
+And I verify the product price is "29"
+When I click on Proceed to checkout button
+Then I should be navigated to sign in page
--- src/test/steps/amazonWebsiteSteps.ts
+++ src/test/steps/amazonWebsiteSteps.ts
@@ -20,49 +20,60 @@ When('I click on Everyday essentials', async function (){
 await this.page.locator("//div[@class='nav-div']//a[text()='Everyday Essentials']").click();
   });
 
-  Then('I should be able to see Grocery', async function (){
-const text1 =  this.page.locator("//a[contains(@class,'dcl-text-carousel-pill') and .//span[normalize-space()='Grocery']]");
+Then('I should be able to see Groceries', async function (){
+ const text1 =  this.page.locator("//span[text()=' Groceries ']");
  const textValue = await text1.textContent();
-expect(textValue).toContain('Grocery');
+ expect(textValue).toContain('Groceries');
   });
 
-When('I click on Grocery', async function (){
-  // await this.page.waitForTimeout(30000);
-await this.page.locator("//a[contains(@class,'dcl-text-carousel-pill') and .//span[normalize-space()='Grocery']]").click();
+When('I click on Groceries', async function (){
+await this.page.locator("//span[text()=' Groceries ']").click();
   });
 
-Then('I should be able to see Grocery page', async function (){
-  const text3 = this.page.locator("//span[@class='acsWidgetCarouselV1__title' and text()='Beverages']");
+Then('I should be navigated to Grocery page', async function (){
+  const text3 = this.page.locator("//h1[text()='Grocery']");
   const textValue1 = await text3.textContent();
-  expect(textValue1).toContain('Beverages');
+  expect(textValue1).toContain('Grocery');
   
 });
 
 When('I select the product',async function (){
-  // await this.page.waitForTimeout(30000);
-  await this.page.locator("//li[@aria-roledescription='slide' and .//span[text()='Red Bull Energy Drink 250 ml (Pack of 4)']]").click();
+  await this.page.locator("//div[@data-csa-c-item-id='amzn1.asin.1.B0FKTT9145']").click();
   ;
 });
 
 Then('I should be navigated to product details page',async function (){
-  // await this.page.waitForTimeout(30000);
-await expect(this.page).toHaveURL(/\/dp\/B07MFYHPNK/);
+await expect(this.page).toHaveURL(/\/dp\/B0FKTT9145/);
 // expect(this.page).toHaveURL(URL);  
 });
 
 When('I click on add to cart button',async function (){
-  // await this.page.waitForTimeout(20000);
-  await this.page.locator("//input[@name='submit.addToCart']").click();
+  await this.page.locator("//input[@id='add-to-cart-button']").click();
 });
 
 Then('I should be able to see the product in cart', async function (){
-  const img = this.page.locator("//div[@data-asin='B07MFYHPNK']");
+  const img = this.page.locator("//div[@data-asin='B0FKTT9145']");
   expect(img).toBeVisible({ timeout: 20000 });
 });
 
 
 
+Then('I verify the product price is {string}', async function(expectedPrice) {
+const actualPrice = await this.page
+    .locator("(//span[@class='a-price-whole'])[1]")
+    .textContent();
+  console.log("Actual Price: " + actualPrice);
+  expect(actualPrice).toContain(expectedPrice);
+});
 
+When('I click on Proceed to checkout button', async function() {
+await this.page.locator("//input[@name='proceedToRetailCheckout']").click();
+})
+
+Then('I should be navigated to sign in page', async function() {
+  await expect(this.page).toHaveURL(/\/ap\/signin/);
+});
+  
 
 
 
